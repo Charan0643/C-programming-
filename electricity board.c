93dbb3a7c58c6beddbae25a2c44f83af3fc7e238
Charan0643 <megacharan88@gmail.com>
2024-11-26 09:43:38 +0530

    Add files via upload

---
+++ electricity board.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char name[10];
+	float unit,charge=100;
+	//clrscr();
+	printf("enter your name and unit consumed:");
+	scanf("%s%f",name,&unit);
+	if(unit<=200)
+	charge=charge+unit*0.80;
+	else if(unit>200&& unit<=300)
+	charge=charge+(200*0.8)+((unit-200)*0.90);
+	else
+	charge=charge+(200*0.80)+(100*0.90)+((unit-300)*1);
+	if(charge>400)
+	charge=charge+(charge*0.15);
+	printf("name:%s\n charge:%f\n",name,charge);
+}
+	
+	
+	
+	
+	
+	
+
+
